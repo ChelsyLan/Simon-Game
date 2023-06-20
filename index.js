08f88c9b72fdcd432a5a988d972e4c63d831fa1b
ChelsyLan <105045192+ChelsyLan@users.noreply.github.com>
2023-06-20 21:28:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -76,22 +76,17 @@ function startOver(){
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("rule-btn");
-
+var ruleBtn = document.getElementById("rule-btn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks on the button, open the modal
-btn.onclick = function () {
+ruleBtn.onclick = function () {
   modal.style.display = "block";
 };
-
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on <span> (x) or anywhere outside the model, close it
 span.onclick = function () {
   modal.style.display = "none";
 };
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
